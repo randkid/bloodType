193e69444fb61fe215c2bd70dd81946a83129a16
Gnlow <entrydark@gmail.com>
2020-07-17 01:08:09 +0900

    change based on randkid/Wrap

--- convert.ts
+++ convert.ts
@@ -1,4 +1,3 @@
-import data from "./data.js"
 import {weightedList} from "https://raw.githubusercontent.com/randkid/Wrap/master/mod.ts"
 
-export default weightedList.getMaterial(data)
+await weightedList.convert("data.csv", "data.js")
